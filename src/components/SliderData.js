aea8db73313df0c085ee51c8f448ae6139cb06eb
Satyam Kumar Das <dsatyam034@gmail.com>
2022-09-14 15:38:20 +0530

    ALL SET  UI_PART

---
+++ src/components/SliderData.js
@@ -0,0 +1,19 @@
+export const SliderData = [
+    {
+        image:'https://images.unsplash.com/photo-1663005955579-fd36d6f02667?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw2MXx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60'       
+    },
+    {
+        image:'https://images.unsplash.com/photo-1663051957237-8da7729a1c15?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwxNXx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60'       
+    },
+    {
+        image:'https://images.unsplash.com/photo-1663011099709-72372cc914c0?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw3N3x8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60'       
+    },
+    {
+        image:'https://images.unsplash.com/photo-1663017587431-abcc495f8b49?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw0N3x8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60'       
+    },
+    {
+        image:'https://images.unsplash.com/photo-1663000216639-a08045401564?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwyNHx8fGVufDB8fHx8&auto=format&fit=crop&w=500&q=60'
+
+    }
+
+];
